ff92a786204c5f79739cfe5894c68b6d42c307a6
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-05-29 01:21:52 -0300

    project settings

---
+++ src/data-source.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import 'reflect-metadata';
+
+import { DataSource } from 'typeorm';
+
+const PORT = process.env.DB_PORT as number | undefined;
+
+
+const AppDataSource = new DataSource({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+});
+
